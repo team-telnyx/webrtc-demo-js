04acf19b635b232c31eb757ea729d489e95240e9
Lucas Rosa <lucas@telnyx.com>
2025-10-16 08:42:18 -0300

    feat: add keepConnectionAliveOnSocketClose call option support (#22)
    * add keepConnectionAliveOnSocketClose call option support
    
    * address beta property
    
    * remove console logs

--- src/components/Call.tsx
+++ src/components/Call.tsx
@@ -3,18 +3,48 @@ import IncomingCall from "./IncomingCall";
 import ActiveCall from "./ActiveCall";
 import ConnectingCall from "./ConnectingCall";
 import HeldCall from "./HeldCall";
+import { capitalizeFirstLetter } from "@/lib/string";
 
 export const Call = () => {
   const [notification] = useTelnyxNotification();
   if (!notification || !notification.call) return null;
 
+  // @ts-expect-error beta option
+  if (notification.call.options.keepConnectionAliveOnSocketClose) {
+    switch (notification.call.state) {
+      case "trying":
+      case "requesting": {
+        return <IncomingCall call={notification.call} />;
+      }
+
+      case "new":
+      case "ringing":
+      case "connecting":
+      case "active": {
+        return (
+          <ActiveCall
+            call={notification.call}
+            title={`${capitalizeFirstLetter(notification.call.state)} Call`}
+          />
+        );
+      }
+
+      case "held": {
+        return <HeldCall call={notification.call} />;
+      }
+      default: {
+        console.error("Unknown call state:", notification.call.state);
+        return null;
+      }
+    }
+  }
+
   switch (notification.call.state) {
-    case "connecting":
     case "trying": {
       return <ConnectingCall call={notification.call} />;
     }
     case "ringing":
-    case "requesting ": {
+    case "requesting": {
       return <IncomingCall call={notification.call} />;
     }
 
