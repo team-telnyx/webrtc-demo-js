e4a981447dc8d9ff8a4cf89cd4b0a1e63c2a8e6d
ArtemPapazian <artemp@telnyx.com>
2025-12-03 18:06:54 +0100

    feat: add validation to TURN servers form field

--- src/components/TurnServersFormField.tsx
+++ src/components/TurnServersFormField.tsx
@@ -7,7 +7,6 @@ import {
   FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input";
-import { cn } from "@/lib/utils";
 
 export function TurnServersFormField<
   TFieldValues extends FieldValues = FieldValues,
@@ -20,6 +19,14 @@ export function TurnServersFormField<
   return (
     <FormField
       {...props}
+      rules={{
+        validate: (value) => {
+          if ((value?.username || value?.password) && !value?.urls?.trim()) {
+            return "TURN Server URL is required when username or password is provided";
+          }
+          return true;
+        },
+      }}
       name={props.name || ("turnServers" as TName)}
       render={({ field }) => {
         const turnServer = Array.isArray(field.value)
@@ -35,45 +42,47 @@ export function TurnServersFormField<
           field.onChange({ ...current, [key]: value });
         };
         return (
-          <div
-            className={cn("grid md:grid-cols-3 gap-4", props.wrapperClassName)}
-          >
-            <FormItem>
-              <FormLabel>TURN Server URL</FormLabel>
-              <FormControl>
-                <Input
-                  data-testid="input-turn-server-url"
-                  placeholder="turn:turn.telnyx.com:3478?transport=tcp"
-                  value={(turnServer?.urls as string) ?? ""}
-                  onChange={(e) => updateTurnServer("urls", e.target.value)}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-            <FormItem>
-              <FormLabel>TURN Username</FormLabel>
-              <FormControl>
-                <Input
-                  data-testid="input-turn-username"
-                  placeholder="Username"
-                  value={turnServer?.username ?? ""}
-                  onChange={(e) => updateTurnServer("username", e.target.value)}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-            <FormItem>
-              <FormLabel>TURN Password</FormLabel>
-              <FormControl>
-                <Input
-                  data-testid="input-turn-password"
-                  placeholder="Password"
-                  value={turnServer?.password ?? ""}
-                  onChange={(e) => updateTurnServer("password", e.target.value)}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
+          <div className={props.wrapperClassName}>
+            <div className="grid md:grid-cols-3 gap-4 mb-1">
+              <FormItem>
+                <FormLabel>TURN Server URL</FormLabel>
+                <FormControl>
+                  <Input
+                    data-testid="input-turn-server-url"
+                    placeholder="turn:turn.telnyx.com:3478?transport=tcp"
+                    value={(turnServer?.urls as string) ?? ""}
+                    onChange={(e) => updateTurnServer("urls", e.target.value)}
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel>TURN Username</FormLabel>
+                <FormControl>
+                  <Input
+                    data-testid="input-turn-username"
+                    placeholder="Username"
+                    value={turnServer?.username ?? ""}
+                    onChange={(e) =>
+                      updateTurnServer("username", e.target.value)
+                    }
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel>TURN Password</FormLabel>
+                <FormControl>
+                  <Input
+                    data-testid="input-turn-password"
+                    placeholder="Password"
+                    value={turnServer?.password ?? ""}
+                    onChange={(e) =>
+                      updateTurnServer("password", e.target.value)
+                    }
+                  />
+                </FormControl>
+              </FormItem>
+            </div>
+            <FormMessage />
           </div>
         );
       }}
