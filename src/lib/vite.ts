4feb923dbb20ef2fddfac9c3f27b034f35fcc733
lucasassisrosa <lucas@telnyx.com>
2026-02-02 18:45:53 -0300

    fix: keep IS_PROD_ENV unchanged

--- src/lib/vite.ts
+++ src/lib/vite.ts
@@ -5,4 +5,4 @@ export const IS_PROD_MODE = import.meta.env.MODE === 'production';
 // VITE_RTC_ENV can be set to 'development' to use rtcdev.telnyx.com
 // Set this env var when building for webrtcdev.telnyx.com
 export const IS_DEV_ENV = import.meta.env.DEV || import.meta.env.VITE_RTC_ENV === 'development';
-export const IS_PROD_ENV = import.meta.env.PROD && import.meta.env.VITE_RTC_ENV !== 'development';
+export const IS_PROD_ENV = import.meta.env.PROD;
